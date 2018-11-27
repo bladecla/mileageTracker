abd202ceb48398f95fc8ffceb2cd3426b88842a5
bladecla <the.bkoku@gmail.com>
2018-11-27 02:26:39 -0600

    moved state up, abstracted from and deleted TripPane

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import TripPane from './TripPane';
+import Pane from './Pane';
 
 class Dashboard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            trips: ["trip 1", "trip 2"]
+        }
+    }
+    addTrip = () => {
+        let newTrip = prompt("New trip: ", "trip " + (this.state.trips.length + 1));
+        this.setState({
+            trips: [...this.state.trips, newTrip]
+        })
+    }
+
     render(){
         return (
             <div>
-                <TripPane/>
+                <Pane title={"Trips"} addChild={this.addTrip}>
+                    {this.state.trips.map((trip, idx) => <div key={idx}>{trip}</div>)}
+                </Pane>
             </div>
         );
     }
