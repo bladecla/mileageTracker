e92237338abdda0cbc371d9aede5772721fce522
bladecla <the.bkoku@gmail.com>
2018-12-13 03:41:07 -0600

    started Insights pane

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,17 +6,32 @@ import TripForm from './TripForm';
 import { connect } from 'react-redux';
 import { addTrip, getTrips, deleteTrip, updateTrip } from './../redux/actions/tripActions'
 import { addVehicle, getVehicles} from './../redux/actions/vehicleActions';
+import Insights from './Insights';
 
 
 class Dashboard extends Component {
   constructor(props){
     super(props);
-    this.state = {
-        
-    }
+    this.state = this.calculateInsights();
+    
   }
   
   toggleTripModal = () => this.setState({ isTripModalOpen: !this.state.isTripModalOpen });
+  calculateInsights = () => {
+    let businessTrips = 0;
+    let totalMileage = 0;
+    const {trips} = this.props.trips;
+
+    trips.forEach(trip => {
+      if (trip.isBusiness) businessTrips++;
+      totalMileage += trip.end - trip.start;
+    })
+
+    return {
+      totalMileage,
+      percentBusiness: (businessTrips / trips.length) * 100
+    }
+  }
   
   render(){
     const { trips } = this.props.trips;
@@ -24,6 +39,7 @@ class Dashboard extends Component {
     const { addTrip, deleteTrip, updateTrip, addVehicle } = this.props;
     return (
       <div id="dash">
+        <Insights {...this.state}/>
         <Pane title="Trips" addChild={this.toggleTripModal}>
           {trips.map((trip) => <Trip key={trip._id} {...trip} delete={deleteTrip} update={updateTrip} addVehicle={addVehicle} vehicles={vehicles}/>)}
         </Pane>
