fd61303435aeea0e5e52677abfeb4b52563604a3
bladecla <the.bkoku@gmail.com>
2018-12-30 03:10:36 -0600

    vehicle api bug fixes

--- routes/api/vehicles.js
+++ routes/api/vehicles.js
@@ -3,7 +3,7 @@ const User = require('./../../models/User');
 const checkAuth = require('./../../auth/check-auth');
 const router = express.Router();
 
-const callback = (req, res) => (err, vehicles) => {
+const callback = res => (err, vehicles) => {
   if (err) {
     console.error(err);
     res.send("Server error")
@@ -16,4 +16,6 @@ router.route("/")
   .get(checkAuth, (req, res) => User.getVehicles(req.user._id, callback(res)))
   .post(checkAuth, (req, res) => User.addVehicle(req.user._id, req.body.vehicle, callback(res)))
   .put(checkAuth, (req, res) => User.updateVehicle(req.user._id, req.body.vehicle, req.body.newVehicle, callback(res)))
-  .delete(checkAuth, (req, res) => User.deleteVehicle(req.user._id, req.body.vehicle))
+  .delete(checkAuth, (req, res) => User.deleteVehicle(req.user._id, req.body.vehicle, callback(res)))
+
+module.exports = router;
